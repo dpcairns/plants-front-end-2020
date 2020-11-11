32498412cc8ce7f7e191bbdd53576d6e6a34ec17
harriet-the-spy <dpcairns@gmail.com>
2020-11-11 13:52:16 -0800

    add destructuring

--- src/Plants.js
+++ src/Plants.js
@@ -14,39 +14,53 @@ export default class Plants extends Component {
     }
 
     fetchPlants = async () => {
+        const { token } = this.props;
+
         await this.setState({ loading: true });
         const response = await request.get('https://plant-dani-plant-2020.herokuapp.com/api/plants')
-        .set('Authorization', this.props.token)
+        .set('Authorization', token)
 
         await this.setState({ plants: response.body, loading: false })
     }
 
     handleSubmit = async (e) => {
+        const {  coolFactor, plantName } = this.state;
+        const { token } = this.props;
+
         e.preventDefault();
 
         const newPlant = {
-            name: this.state.plantName,
-            cool_factor: this.state.coolFactor,
+            name: plantName,
+            cool_factor: coolFactor,
         };
         
         await this.setState({ loading: true });
 
         await request.post('https://plant-dani-plant-2020.herokuapp.com/api/plants')
         .send(newPlant)
-        .set('Authorization', this.props.token)  ;
+        .set('Authorization', token)  ;
 
         await this.fetchPlants();
     }
 
     handleWaterClick = async (someId) => {
+        const { token } = this.props;
+
         // let's update a particular plant (we'll need its id)
         await request.put(`https://plant-dani-plant-2020.herokuapp.com/api/plants/${someId}`)
-        .set('Authorization', this.props.token)  ;
+        .set('Authorization', token)  ;
 
         await this.fetchPlants();
     }
 
     render() {
+        const { 
+            plantName, 
+            coolFactor, 
+            loading,
+            plants,
+         } = this.state;
+
         return (
             <div>
                 Welcome to plants!
@@ -54,7 +68,7 @@ export default class Plants extends Component {
                     <label>
                         Add a plant:
                         <input 
-                            value={this.state.plantName} 
+                            value={plantName} 
                             onChange={(e) => this.setState({ plantName: e.target.value })}
                         />
                     </label>
@@ -62,7 +76,7 @@ export default class Plants extends Component {
                         Cool factor:
                         <input 
                             type="number"
-                            value={this.state.coolFactor} 
+                            value={coolFactor} 
                             onChange={(e) => this.setState({ coolFactor: e.target.value })}
                         />
                     </label>
@@ -71,9 +85,9 @@ export default class Plants extends Component {
                         </button>
                 </form>
                 {
-                    this.state.loading 
+                    loading 
                         ? 'LOADING!!!!!' 
-                        : this.state.plants.map(plant => <div key={`${plant.name}${plant.id}${Math.random()}`} style={{ 
+                        : plants.map(plant => <div key={`${plant.name}${plant.id}${Math.random()}`} style={{ 
                             textDecoration: plant.is_watered ? 'line-through' : 'none' }
                         }>
                         name: {plant.name}
